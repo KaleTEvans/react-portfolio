f7e0ae2b2591a77bfb87439876cf1ae73b5d80ce
KaleTEvans <kaletevans@gmail.com>
2021-07-06 19:48:05 -0500

    completed resume page

---
+++ src/components/Resume/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function Resume() {
+
+    return (
+        <section>
+            <div className='mt-5'>
+                <h5 className='text-center'>
+                    <a href='https://www.dropbox.com/s/mceks8773q5ub0x/Kale_Evans.pdf?dl=0' target='_blank' className='resume'>Click Here for a Copy of My Resume</a>
+                </h5>
+                <h6 className='text-center mt-5 mb-3'>Proficiencies</h6>
+                <ul className='skillset text-center'>
+                    <li className='mt-2 mb-2'>HTML5</li>
+                    <li className='mt-2 mb-2'>CSS3</li>
+                    <li className='mt-2 mb-2'>JavaScript</li>
+                    <li className='mt-2 mb-2'>NodeJs</li>
+                    <li className='mt-2 mb-2'>ExpressJs</li>
+                    <li className='mt-2 mb-2'>MySQL</li>
+                    <li className='mt-2 mb-2'>MongoDB</li>
+                    <li className='mt-2 mb-2'>ReactJs</li>
+                    <li className='mt-2 mb-2'>C++</li>
+                    <li className='mt-2 mb-2'>VSCode</li>
+                    <li className='mt-2 mb-2'>Object Oriented Programming</li>
+                    <li className='mt-2 mb-2'>Test Driven Development</li>
+                    <li className='mt-2 last-skill'>Bootstrap</li>
+                </ul>
+            </div>
+        </section>
+    )
+
+}
+
+export default Resume;
